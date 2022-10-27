8f5505e82c3eb30045462177fd0c4064168ba6a4
Daniel Pett <dejp3@cam.ac.uk>
2022-10-27 19:05:55 +0100

    Add pagination

---
+++ src/components/structure/pagination.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropType from "prop-types";
+import {Link} from "gatsby";
+import {Container} from "react-bootstrap";
+
+export default function Pagination(props) {
+
+    const {pageContext} = props;
+    const {pageNumber, numberOfPages, humanPageNumber, previousPagePath, nextPagePath} = pageContext;
+    return (
+            <Container>
+                <nav className="pagination py-2">
+                    {humanPageNumber > 1 ? (
+                            <Link
+                                title="Go to previous page"
+                                to={previousPagePath}>
+                                ← Newer posts
+                            </Link>) :
+                        <span/>}
+                    <strong>Page {pageNumber + 1} of {numberOfPages}</strong>
+                    {humanPageNumber < numberOfPages ? (
+                            <Link
+                                title="Go to next page"
+                                to={nextPagePath}>
+                                Older posts →
+                            </Link>) :
+                        <span/>}
+                </nav>
+            </Container>
+    )
+
+}
+
+
+Pagination.propType = {
+    pageContext: PropType.object.isRequired,
+}
