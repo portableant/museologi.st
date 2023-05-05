4da73bc2561c03073988c82cb87fe4d33507da80
Daniel Pett <danielpett@gmail.com>
2023-05-05 09:56:45 +0100

    Chore: Update packages

--- src/components/structure/post-card.js
+++ src/components/structure/post-card.js
@@ -4,18 +4,21 @@ import {Link} from "gatsby"
 import {GatsbyImage, getImage} from "gatsby-plugin-image";
 
 const PostCard = ({post}) => (
-    <Col md={4} className="mb-3 aos-init aos-animate" data-aos-duration="500" data-aos="fade-up-right"  data-aos-easing="linear" data-aos-delay="0">
+    <Col md={4} className="mb-3">
         <Card className="border-0 bg-light">
             <CardImg className="card-img-top rounded-0"
                      as={GatsbyImage} image={getImage(post.frontmatter.featuredImg)} alt={post.frontmatter.title}/>
             <Card.Body className="bg-white border-0 rounded-0">
-                <div className="h-100" style={{minHeight: 70}}>
+                <div >
                     <Link to={post.frontmatter.slug} className="stretched-link stretched-link__blog_post">
                         <h1 className="lead text-black fw-bold">{post.frontmatter.title}</h1>
                     </Link>
                 </div>
+                {post.frontmatter.date && <div>
+                    <h4 className="text-info">{post.frontmatter.date}</h4>
+                </div>}
                 {post.frontmatter.institution && <div className="h-100">
-                    <Badge bg="primary" className="text-white p-2">{post.frontmatter.institution}</Badge>
+                    <Badge bg="dark" className="text-white p-2">{post.frontmatter.institution}</Badge>
                 </div>}
             </Card.Body>
         </Card>
