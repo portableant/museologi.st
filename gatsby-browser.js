2ac89be0a41574c49c504c48a41a79874280eae0
Daniel Pett <danielpett@gmail.com>
2025-08-04 08:32:42 +0100

    Refactor import statements in gatsby-browser.js to use CommonJS require() syntax

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,18 +4,25 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
-import "@popperjs/core/dist/umd/popper.min"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min"
-import "./src/styles/global.css"
-import "@fontsource/public-sans";
-import 'typeface-sorts-mill-goudy/index.css';
-import './static/plyr.css';
-import {defineCustomElements as deckDeckGoHighlightElement} from '@deckdeckgo/highlight-code/dist/loader';
+// All import statements have been converted to CommonJS require() calls.
+
+require("@popperjs/core/dist/umd/popper.min");
+require("bootstrap/dist/css/bootstrap.min.css");
+require("bootstrap/dist/js/bootstrap.min");
+require("./src/styles/global.css");
+require("@fontsource/public-sans");
+require("typeface-sorts-mill-goudy/index.css");
+require("./static/plyr.css");
 require("prismjs/themes/prism-tomorrow.css");
 
+// The deckDeckGoHighlightElement function is also imported using require().
+// Note that its import syntax is a bit different because it's a dynamic ES6 import,
+// so we need to use a slightly different approach to get the loader.
+const { defineCustomElements: deckDeckGoHighlightElement } = require('@deckdeckgo/highlight-code/dist/loader');
+
 deckDeckGoHighlightElement();
+
+// This shouldUpdateScroll function is already in the correct CommonJS format.
 exports.shouldUpdateScroll = ({ routerProps: { location } }) => {
   // Check if there is a hash in the URL. If there is, it's likely an anchor link,
   // so we'll let the browser handle the scroll.
